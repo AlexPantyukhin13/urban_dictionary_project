3d962cfc57abbb8c689f19a700050cfb0219f52f
Aleksandr Pantiukhin <sanyapantyukhin@gmail.com>
2019-10-05 13:27:58 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import urbandictionary as ud
+import random
+
+import telebot
+
+bot_key = "741204635:AAFFVeaEgn53bWGTndZuJPTMjXxu-SLbSpk"
+chat_id = "-1001378894764"
+
+bot = telebot.TeleBot(bot_key)
+
+
+def get_random_definition():
+    random_definitions = ud.random()
+    one_definition = random.choice(random_definitions)
+    return f"{one_definition.word.upper()}: {one_definition.definition}"
+
+
+def send_message(chat_id, text):
+    bot.send_message(chat_id, text)
+
+def app():
+    send_message(chat_id=chat_id, text=get_random_definition())
+
+
+if __name__ == '__main__':
+    app()
